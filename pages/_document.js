12785f9ea732be304350456c4bb670382e9a9b7e
Kashyap Patel <khpatel4991@users.noreply.github.com>
2019-02-03 00:00:36 -0800

    Add emotion

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,19 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import { extractCritical } from "emotion-server";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const page = ctx.renderPage();
+    const styles = extractCritical(page.html);
+    return { ...page, ...styles };
+  }
+
+  constructor(props) {
+    super(props);
+    const { __NEXT_DATA__, ids } = props;
+    if (ids) {
+      __NEXT_DATA__.ids = ids;
+    }
   }
 
   render() {
@@ -14,10 +24,11 @@ export default class MyDocument extends Document {
           <meta name="description" content="Vested finance" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
           <link rel="icon" href="/static/assets/favicon.ico" />
           <link rel="manifest" href="/static/assets/site.webmanifest" />
         </Head>
-        <body className="custom_class">
+        <body>
           <Main />
           <NextScript />
         </body>
